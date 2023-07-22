ce8fa54de8a608e339023408694e67d8d4e860fd
Frank Parra <panf9188@gmail.com>
2023-07-21 21:55:18 -0500

    Creamos el formulario para la carga de datos nuevos

---
+++ semana08-2/dia03-javascript-ec6-fetch-crud/public/services.js
@@ -0,0 +1,9 @@
+export const fetchMovies = async () => {
+    const url = 'http://localhost:3000/movies?_expand=genre'
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    return data
+}
