c4ed3c28c512997c939451fd0dea0341255cb884
Frank Parra <panf9188@gmail.com>
2023-07-27 21:12:07 -0500

    Mostrando solo los títulos de cada objeto

--- semana09/dia02-react-todo-app/src/App.jsx
+++ semana09/dia02-react-todo-app/src/App.jsx
@@ -38,16 +38,36 @@ function App() {
   
   return (
     <>
-      <h1 className="text-6xl font-bold underline">
-        TODO APP
-      </h1>
+      <main className="w-full max-w-sm mx-auto mt-10 rounded-lg bg-yellow-100 border border-yellow-400 p-4 shadow-sm">
+        <h1 className="text-2xl font-bold ">
+          TODO APP
+        </h1>
+        <input type="text" 
+          className="w-full border my-3 p-3" 
+          placeholder="¿Qué deseas hacer hoy?"
+        />
 
-      {/* {JSON.stringify(todos)} */}
-      <ul>
-        {todos.map(todo => {
-          return <li key={todo.id}>{todo.title}</li>
-        })}
-      </ul>
+        <section className="mt-5">
+        {/* {JSON.stringify(todos)} */}
+        <ul className="flex flex-col gap-3">
+          {todos.map(todo => {
+            return (<li 
+                key={todo.id} 
+                className={`
+                  text-sky-600 
+                  bg-sky-200 
+                  p-2 
+                  rounded-md 
+                  ${todo.completed? 'line-through':''}`} 
+              >
+                <input type="checkbox" className="mr-4"/>
+                  <span>{todo.title}</span>
+              </li>
+            )
+          })}
+        </ul>
+        </section>
+      </main>
     </>
   )
 }
