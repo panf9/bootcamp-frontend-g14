9f7b9c5588e104ac29d52669559d582dc549924a
Frank Parra <panf9188@gmail.com>
2023-08-16 22:20:55 -0500

    Creamos el Login y validamos el dato del servidor

--- semana12/dia01-react-budget-app-custom-hooks-contetext/src/pages/Login.jsx
+++ semana12/dia01-react-budget-app-custom-hooks-contetext/src/pages/Login.jsx
@@ -1,7 +1,88 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-  return (
-    <div>Login</div>
+    const [form, setForm] = useState({
+      email: '',
+      password: '',
+    })
+  
+    const navigate = useNavigate()
+    const handleChange = (event) => {
+      const value = event.target.value 
+      const name = event.target.name
+  
+      setForm({ 
+        ...form, 
+        [name]:value
+      })
+    }
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+
+      const email = form.email
+      const password = form.password
+  
+      const url = `https://64dd6d75825d19d9bfb12825.mockapi.io/api/v1/users?email=${email}&password=${password}`
+  
+      // const options = {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(form)
+      // }
+  
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data);
+  
+      setForm({email: '',password: ''})
+      
+      if (data.length === 1){
+        navigate('/login')
+      }else{
+        alert("credenciales incorrectas!")
+      }
+    }
+  
+    return (
+      <form className="w-96 mx-auto" onSubmit={handleSubmit}>
+        <h1 className="text-center text-3xl mb-8">Budget App - Register</h1>
+        {JSON.stringify(form)}
+        <div className="flex flex-col gap-4 bg-slate-200 p-8 shadow-lg">
+          <label className=" flex flex-col font-medium">
+            Email
+            <input 
+              type="email" 
+              name="email"
+              placeholder="correo@email.com" 
+              className="border f-full p-3" 
+              required
+              onChange={handleChange}
+              value={form?.email}
+            />
+          </label>
+          <label className="flex flex-col font-medium">
+            Password 
+            <input 
+              type="password" 
+              name="password"
+              placeholder="*******" 
+              className="border w-full p-3" 
+              required
+              onChange={handleChange}
+              value={form?.password}
+            />
+          </label>
+          <input 
+            type="submit" 
+            value="Register" 
+            className="w-full bg-amber-400 p-3 font-medium"
+          />
+        </div>
+      </form>
   )
 }
 
